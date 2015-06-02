a2e0f6ea3979e8a4d36d38c8ef4349a9fbcf80f3
Ed O'Brien <edmund.obrien.2010@live.rhul.ac.uk>
2015-06-02 13:42:16 +0100

    changed files for heroku

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+var port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/'));
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, './', "./index.html"));
 });
 
-
-
-server.listen(3000, function(){
+server.listen(port, function(){
   console.log('Server Listening on Port 3000')
 });
 
